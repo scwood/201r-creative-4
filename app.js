7d128f3f1df340de9f983449cee254ced86fa223
Spencer Wood <spencercwood@gmail.com>
2017-03-13 19:45:48 -0600

    Add public

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ var app = express();
 
 app.use(express.static(path.resolve(path.join(__dirname, 'public'))));
 
+app.get('/api/timezones', function (req, res) {
+  res.send([]);
+});
+
+app.get('/api/timezones/:timezone', function (req, res) {
+  res.send({});
+});
+
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('listening on port ' + port);
