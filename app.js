0c9684abcad4c0766d13cd050a271d50d280ebda
Spencer Wood <spencercwood@gmail.com>
2017-03-13 19:22:39 -0600

    Add public folder / etc.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var path = require('path');
 var app = express();
+
+app.use(express.static(path.resolve(path.join(__dirname, 'public'))));
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
