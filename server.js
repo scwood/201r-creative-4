437ef6c1470dd8592aaef4263303c68785741378
Spencer Wood <spencercwood@gmail.com>
2017-03-15 18:33:33 -0600

    Add time to timezone api call

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var timezones = require('./timezones.json').timezones;
+var moment = require('moment');
 var app = express();
 
 app.use(express.static(path.resolve(path.join(__dirname, 'public'))));
@@ -17,7 +18,7 @@ app.get('/api/timezones/:abbreviation', function (req, res) {
     return;
   }
   var timezone = timezones[req.params.abbreviation];
-  var time = calculateTimeFromOffset(timezone.offset)
+  timezone.time = moment.utc().utcOffset(timezone.offset).format('ddd MMM Do YYYY, h:mm a')
   res.send({ timezone: timezone });
 });
 
@@ -29,10 +30,6 @@ app.use(function (error, req, res, next) {
   }
   res.status(500).send({ error: 'Internal server error'});
 });
-
-function calculateTimeFromOffset(offset) {
-  console.log(offset)
-}
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
